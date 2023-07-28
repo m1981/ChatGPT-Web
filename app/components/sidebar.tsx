2de363b48fc1475139706654ebf5402a92d8d1bb
Michal <m.nakiewicz@gmail.com>
2023-07-28 22:32:03 +0200

    Alice introduced

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -86,9 +86,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Alice</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Chat & Explore With Your AI Friend.
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
