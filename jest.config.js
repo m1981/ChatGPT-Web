550e2f698004e2349d66cecd8a3232dd01185d1d
Michal <m.nakiewicz@gmail.com>
2023-11-18 12:24:14 +0100

    fix  for ChatItem tests: Simple mock rather than transforming the SVG files into components for your tests, you can remove the jest-svg-transformer configuration and continue with the simple mocking approach that we have discussed before.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,24 @@
 // jest.config.js
-transform: {
-  '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-},
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
+    // Add other mappings for CSS modules if necessary:
+    '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
+    // Continue your existing mappings here
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    // Add other transformations if necessary
+  },
+  // Add other Jest configuration options here
+};
+
+module.exports = createJestConfig(customJestConfig);
