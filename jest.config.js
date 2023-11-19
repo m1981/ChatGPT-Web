820f1b227c183de017ad33246441f1ef7a6fbd37
Michal <m.nakiewicz@gmail.com>
2023-11-19 23:47:28 +0100

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -2,29 +2,20 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: './', // Specify the base directory of your project
+  dir: './',
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Location of your jest.setup.js file
-  testEnvironment: 'jest-environment-jsdom', // Use jsdom environment
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    // Handle CSS imports (assuming you have CSS modules)
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    // Handle image imports, if necessary
-    '\\.(jpg|jpeg|png|gif|webp|avif)$': '<rootDir>/__mocks__/fileMock.js',
-    // Mock SVGs
-    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js', // Adjusted to match your mock file path
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
   },
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // This is necessary for next.js projects
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'], // Ignore certain folders from being tested
-  collectCoverageFrom: ['<rootDir>/app/components/**/*.tsx', '!<rootDir>/node_modules/', '!<rootDir>/.next/'], // Collect coverage from files, if necessary
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: ['<rootDir>/app/components/**/*.tsx', '!<rootDir>/node_modules/', '!<rootDir>/.next/'],
 };
 
-// Create the Jest config with the next/jest preset and your custom configuration
 module.exports = createJestConfig(customJestConfig);
